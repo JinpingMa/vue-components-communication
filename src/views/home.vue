55f0780de101b265453c0607b2455e5f92cfde9d
majinping <hyperbola615@gmail.com>
2019-06-05 18:40:56 +0800

    add router, aliases config and lint config file

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data () {
+    return {
+      jkjdk: ''
+    }
+  },
+  created () {
+    debugger
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
